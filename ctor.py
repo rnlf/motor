a0f3a77799e76743cf7ba8f056c38fcd894fd0a5
rnlf <rnlf@users.noreply.github.com>
2015-09-02 12:51:21 +0200

    intermediate commit

--- ctor.py
+++ ctor.py
@@ -40,31 +40,125 @@ def add_prebuild_step(func, outputs=[], dependencies=[]):
   prebuild.append(func)
 
 
+
+class CompileFile():
+  def __init__(self, src):
+    self.source = src
+    self.actions = []
+    self.properties = {}
+
+  def add_compiler_flags(self, *newflags):
+    if "CFLAGS" not in self.properties:
+      self.properties["CFLAGS"] = []
+
+    for _, f in enumerate(newflags):
+      self.properties["CFLAGS"].append(f)
+
+  def add_include_dirs(self, *dirs):
+    if "INCLUDE_DIRS" not in self.properties:
+      self.properties["INCLUDE_DIRS"] = []
+
+    for _, d in enumerate(dirs):
+      self.properties["INCLUDE_DIRS"].append(d)
+
+  def add_required_action(self, action):
+    self.actions.append(action)
+
+
+class Module:
+  def __init__(self, source_dir):
+    self.files = {}
+    self.properties = {}
+    self.source_dir = source_dir
+    self.pre_actions = []
+
+  def read_definition(self, definition):
+    self.version = definition["version"]
+
+
+  def add_files(self, *files):
+    for _, f in enumerate(*files):
+      print("FILE: " + f)
+      self.files[f] = CompileFile(os.path.join(self.source_dir, f))
+
+  def add_required_action(self, action):
+    self.pre_actions.append(action)
+
+  def add_compiler_flags(self, *newflags):
+    if "CFLAGS" not in self.properties:
+      self.properties["CFLAGS"] = []
+
+    for _, f in enumerate(newflags):
+      self.properties["CFLAGS"].append(f)
+
+  def add_include_dirs(self, *dirs):
+    if "INCLUDE_DIRS" not in self.properties:
+      self.properties["INCLUDE_DIRS"] = []
+
+    for _, d in enumerate(dirs):
+      self.properties["INCLUDE_DIRS"].append(d)
+
+  def add_file_compiler_flags(self, filename, *newflags):
+    self.files[filename].add_compiler_flag(newflags)
+
+  def add_file_include_dirs(self, filename, *newdirs):
+    self.files[filename].add_include_dirs(newdirs)
+
+  def add_file_required_action(self, action):
+    self.files[filename].add_required_action(action)
+
+
 def load_modules(repo_directories):
   modules = {}
     
-  def load_module_file(filename):
+  def load_module_file(source_dir, filename):
     print("Reading module " + fname)
+
+    module = Module(source_dir)
+
+    def sd():
+      return source_dir
+
+    def aid(*dirs):
+      return module.add_include_dirs(dirs)
+
+    def acf(*flags):
+      return module.add_compiler_flags(flags)
+
+    def af(*files):
+      return module.add_files(files)
+
+    def apbs(step):
+      return module.add_required_action(step)
+
+    def afid(f, *dirs):
+      return module.add_file_include_dirs(f, dirs)
+
+    def afcf(f, *flags):
+      return module.add_file_compiler_flags(f, flags)
+
     with open(fname) as f:
       global_vars = {
-        'source_directory':      source_directory,
-        'add_include_dirs':      add_include_dirs,
-        'add_compiler_flags':    add_compiler_flags,
-        'add_files':             add_files,
-        'add_prebuild_step':     add_prebuild_step,
-        'add_file_include_dirs': add_file_include_dirs,
-        'add_file_flags':        add_file_flags,
+        'source_directory':      sd,
+        'add_include_dirs':      aid,
+        'add_compiler_flags':    acf,
+        'add_files':             af,
+        'add_prebuild_step':     apbs,
+        'add_file_include_dirs': afid,
+        'add_file_flags':        afcf,
         'os':                    os
       }
-      module = {}
+      module_def = {}
 
       code = compile(f.read(), filename, 'exec')
-      exec(code, global_vars, module)
-      if module["name"] in modules:
-        if module["version"] < modules[module["name"]]["version"]:
+      exec(code, global_vars, module_def)
+      module.read_definition(module_def)
+      if module_def["name"] in modules:
+        if module_def["version"] < modules[module_def["name"]].version:
           return
 
-      modules[module["name"]] = module
+      modules[module_def["name"]] = module
+      module_def["configure"]({})
           
 
   for d in repo_directories:
@@ -76,7 +170,7 @@ def load_modules(repo_directories):
     print(repo["repository"], repo["source_dir"])
 
     for fname in glob.iglob(os.path.join(d, "modules", "*.ctor")):
-      load_module_file(fname)
+      load_module_file(repo["source_dir"], fname)
 
   return modules
 
